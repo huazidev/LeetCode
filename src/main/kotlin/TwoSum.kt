12cd4ed2f7c57fc0eb92b2bfd90e0bfc793e9f80
huazidev <huazi.dev@gmail.com>
2020-04-06 10:55:18 +0800

    feat: add TwoSum solutions

--- src/main/kotlin/TwoSum.kt
+++ src/main/kotlin/TwoSum.kt
@@ -1,14 +1,50 @@
 /**
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
  * @author hua on 2020/01/03.
  */
+class TwoSum {
+    // solution one
+    fun twoSum1(nums: IntArray, target: Int): IntArray {
+        nums.forEachIndexed { index, _ ->
+            for (i in (index + 1)..nums.size) {
+                if (nums[index] + nums[i] == target) {
+                    return intArrayOf(index, i)
+                }
+            }
+        }
+        return IntArray(2)
+    }
 
-class Solution {
+    // solution two
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        return IntArray(1)
+        // key is target minus value of nums, value is indices of nums
+        val resultMap = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, value ->
+            if (resultMap.containsKey(value)) {
+                val anotherIndex: Int = resultMap[value]!!
+                return intArrayOf(anotherIndex, index)
+            } else {
+                resultMap[target - value] = index
+            }
+        }
+        // no result, return empty array
+        return IntArray(2)
     }
 }
 
 fun main() {
+    val nums = intArrayOf(2, 7, 11, 15)
+    val result1 = TwoSum().twoSum1(nums, 9)
+    println("result1: ${result1.joinToString()}")
 
-
+    val result = TwoSum().twoSum(nums, 9)
+    println("result: ${result.joinToString()}")
 }
